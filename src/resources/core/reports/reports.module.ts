b4f3f38b8adad341357303e3d3d05f7f7ead4eb6
Yan Gonçalves <fgyan94@gmail.com>
2022-10-31 18:17:33 -0300

    feat: inject ReportsService into ReportsModule

--- src/resources/core/reports/reports.module.ts
+++ src/resources/core/reports/reports.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ReportsController } from './reports.controller'
+import { ReportsService } from './reports.service'
 
 @Module({
   imports: [],
   controllers: [ReportsController],
-  providers: [],
+  providers: [ReportsService],
   exports: []
 })
 export class ReportsModule {}
