70a1d7ec8c8fb0398cbea01d48bf720c35c2edb2
Yan Gonçalves <fgyan94@gmail.com>
2022-10-31 18:18:50 -0300

    feat: injects UsersService into ReportsService

--- src/resources/core/reports/reports.service.ts
+++ src/resources/core/reports/reports.service.ts
@@ -1,4 +1,7 @@
+import { UsersService } from '@core/users/users.service'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(private readonly usersService: UsersService) {}
+}
