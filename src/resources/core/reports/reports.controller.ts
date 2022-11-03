75f7b75f16125a8eb42f2707cd1112519e0de04e
Yan Gonçalves <fgyan94@gmail.com>
2022-11-02 23:25:22 -0300

    feat: add ReportsController

--- src/resources/core/reports/reports.controller.ts
+++ src/resources/core/reports/reports.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post, StreamableFile } from '@nestjs/common'
+import { ReportDto } from './dto/report.dto'
 import { ReportsService } from './reports.service'
 
-@Controller('report')
+@Controller('reports')
 export class ReportsController {
   constructor(private readonly service: ReportsService) {}
+
+  @Post()
+  async generate(@Body() input: ReportDto): Promise<{ uuid: string }> {
+    return await this.service.generate(input)
+  }
+
+  @Get(':uuid')
+  async download(@Param('uuid') uuid: string): Promise<StreamableFile> {
+    return await this.service.download(uuid)
+  }
 }
